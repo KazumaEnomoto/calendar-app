2fd75d959a5b7f8b817df99d3d89c72cd9b762fb
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-07-23 10:18:25 +0900

    曜日を表示

--- calendar.js
+++ calendar.js
@@ -4,11 +4,20 @@ let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 
 function displayCalendar() {
+    //htmlの構造を定義
     const header = document.querySelector(".header");
     const weekdaysContainer = document.querySelector(".weekdays");
     const daysContainer = document.querySelector(".days");
-
+    
+    //現在の年と月を表示
     header.textContent = `${currentYear}年 ${currentMonth + 1}月`;
+
+    //曜日を表示
+    weekdays.forEach((day) => {
+        const weekdayElement = document.createElement("div");
+        weekdayElement.textContent = day;
+        weekdaysContainer.appendChild(weekdayElement);
+    })
 }
 
 displayCalendar();
