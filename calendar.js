5fc43a0713831fd99c1b3ab92e321dcf2367e9f7
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-07-31 13:31:58 +0900

    displayCalendar関数を表示と作成に要素分け（元の関数はコメントで残す）

--- calendar.js
+++ calendar.js
@@ -33,6 +33,32 @@ const createCalendar = () => {
 }
 
 const displayCalendar = () => {
+    //htmlの構造を定義
+    const header = document.querySelector(".header");
+    const weekdaysContainer = document.querySelector(".weekdays");
+    const daysContainer = document.querySelector(".days");
+
+    //現在の年と月を表示
+    header.textContent = `${currentYear}年 ${currentMonth + 1}月`;
+
+    //曜日を表示
+    const weekdaysElement = weekdays.map((day) => {
+        const weekdayElement = document.createElement("div");
+        weekdayElement.textContent = day;
+        return weekdayElement;
+    })
+    weekdaysContainer.append(...weekdaysElement);
+
+     //月の初日と最終日を取得
+     const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+     const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);//月の最終日は翌月の0日目を取得する
+     const lastDayOfPrevMonth = new Date(currentYear, currentMonth, 0);
+ 
+     const days = generateDays(firstDayOfMonth, lastDayOfMonth, lastDayOfPrevMonth);
+     daysContainer.append(...days);
+}
+
+/*const displayCalendar = () => {
     const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
     const today = new Date();
     const currentYear = today.getFullYear();
@@ -53,12 +79,6 @@ const displayCalendar = () => {
         return weekdayElement;
     })
     weekdaysContainer.append(...weekdaysElement);
-    //元々書いていた関数｜曜日を表示（学習用に記載）
-    /*weekdays.forEach((day) => {
-        const weekdayElement = document.createElement("div");
-        weekdayElement.textContent = day;
-        weekdaysContainer.appendChild(weekdayElement);
-    })*/
 
     //月の初日と最終日を取得
     const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
@@ -67,23 +87,7 @@ const displayCalendar = () => {
 
     const days = generateDays(firstDayOfMonth, lastDayOfMonth, lastDayOfPrevMonth);
     daysContainer.append(...days);
-
-    //元々書いていた定数｜前月の日数を取得（学習用に記載）
-    //const lastDayOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
-
-    //元々書いていた関数｜日付を表示（学習用に記載）
-    /*for (let i = firstDayOfMonth.getDay(); i > 0; i--) {//日曜日の日にちから前月の最終日まで繰り返し
-        const dayElement = document.createElement("div");
-        dayElement.textContent = lastDayOfLastMonth - i + 1;
-        daysContainer.appendChild(dayElement);
-    }
-
-    for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-        const dayElement = document.createElement("div");
-        dayElement.textContent = i;
-        daysContainer.appendChild(dayElement);
-    }*/
-}
+}*/
 
 //前月のカレンダーを表示
 const prev = () => {
