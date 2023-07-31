6f4df382a0374c7a11d74a59dbcb7d4f029a145e
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-07-31 13:40:36 +0900

    元に戻した

--- calendar.js
+++ calendar.js
@@ -24,41 +24,8 @@ const generateDays = (firstDay, lastDay, prevLastDay) => {
     return days;
 }
 
-//指定された月のカレンダーを作成
-const createCalendar = () => {
-    const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
-    const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth();
-}
 
 const displayCalendar = () => {
-    //htmlの構造を定義
-    const header = document.querySelector(".header");
-    const weekdaysContainer = document.querySelector(".weekdays");
-    const daysContainer = document.querySelector(".days");
-
-    //現在の年と月を表示
-    header.textContent = `${currentYear}年 ${currentMonth + 1}月`;
-
-    //曜日を表示
-    const weekdaysElement = weekdays.map((day) => {
-        const weekdayElement = document.createElement("div");
-        weekdayElement.textContent = day;
-        return weekdayElement;
-    })
-    weekdaysContainer.append(...weekdaysElement);
-
-     //月の初日と最終日を取得
-     const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
-     const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);//月の最終日は翌月の0日目を取得する
-     const lastDayOfPrevMonth = new Date(currentYear, currentMonth, 0);
- 
-     const days = generateDays(firstDayOfMonth, lastDayOfMonth, lastDayOfPrevMonth);
-     daysContainer.append(...days);
-}
-
-/*const displayCalendar = () => {
     const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
     const today = new Date();
     const currentYear = today.getFullYear();
@@ -87,7 +54,7 @@ const displayCalendar = () => {
 
     const days = generateDays(firstDayOfMonth, lastDayOfMonth, lastDayOfPrevMonth);
     daysContainer.append(...days);
-}*/
+}
 
 //前月のカレンダーを表示
 const prev = () => {
