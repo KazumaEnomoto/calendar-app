f54316f39cfb392ffde76f207ef81ec1a65a5077
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-01 16:09:58 +0900

    login関数追記：ユーザ名とパスワードを取得

--- register.js
+++ register.js
@@ -1,5 +1,8 @@
 //サインアップ・サインイン機能の実装
 
 const login = () => {
-    
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 }
