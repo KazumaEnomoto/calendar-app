4d74d98b2d46263e6979c7eac96d46d37a377da3
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-01 16:26:24 +0900

    authenticateUser関数を作成：ユーザ名とパスワードを認証

--- register.js
+++ register.js
@@ -1,8 +1,20 @@
 //サインアップ・サインイン機能の実装
+const users = [
+    { username: 'user1', password: 'password1'}
+    { username: 'user2', password: 'password2'}
+];
+
 
 const login = () => {
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const username = usernameInput.value;
     const password = passwordInput.value;
+}
+
+const authenticateUser = (username, password) => {
+    const authenticatedUser = users.find(
+        (user) => user.username === username && user.password === password
+    );
+    return authenticatedUser;
 }
