9d4e9f9d5f13857da3b27cc01e335d9c970e5ba2
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-01 16:40:53 +0900

    機能についてのコメントを追加

--- register.js
+++ register.js
@@ -1,28 +1,33 @@
-//サインアップ・サインイン機能の実装
+//ユーザの仮データを作成
 const users = [
     { username: 'user1', password: 'password1'},
     { username: 'user2', password: 'password2'},
 ];
 
-
+//ログイン認証機能
 const login = () => {
+    //フォームから値を取得
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const username = usernameInput.value;
     const password = passwordInput.value;
 
+    //入力された値の認証
     const authenticateUser = authenticateUser(username, password);
 
+    //認証後のアラートおよび処理
     if (authenticateUser) {
         alert('ログインに成功しました。');
     } else {
         alert('ユーザー名もしくはパスワードが間違っています。');
     }
 
+    //入力された値の削除
     usernameInput.value = '';
     passwordInput.value = '';
 }
 
+//フォームから入力された値の認証機能
 const authenticateUser = (username, password) => {
     const authenticatedUser = users.find(
         (user) => user.username === username && user.password === password
