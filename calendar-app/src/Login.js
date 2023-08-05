27e7beccfe8d13a6c3d075eee86337f8f3343e40
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-05 17:14:55 +0900

    フォームで取得する要素の作成

--- calendar-app/src/Login.js
+++ calendar-app/src/Login.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input
+              type="text"
+              placeholder="ユーザー名"
+              name="username"
+            />
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input
+              type="text"
+              placeholder="メールアドレス"
+              name="mailAddress"
+            />
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input
+              type="text"
+              placeholder="パスワード"
+              name="password"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
