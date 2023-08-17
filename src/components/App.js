933cb2fa315edd2dd56ba0e8cc1322078448d958
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-17 23:00:06 +0900

    各ページのRouteを設定

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Login from './Login';
+import Mypage from './Mypage';
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/customerdetail" element={<CustomerDetail />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
     </div>
   )
 }
