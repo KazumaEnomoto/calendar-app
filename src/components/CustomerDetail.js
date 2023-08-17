5fd15c0811c62026e79560018b174ff7ffae6dd2
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-18 01:54:38 +0900

    顧客情報入力フォームを作成（名前、生年月日、年齢のみ）

--- src/components/CustomerDetail.js
+++ src/components/CustomerDetail.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const CustomerDetail = () => {
-  return (
-    <div>CustomerDetail</div>
-  )
+
+
+    return (
+        <div className="formContainer">
+            <div className='formField'>
+                <label>名前</label>
+                <input
+                    type='text'
+                    placeholder='名前'
+                    name='name'
+                />
+            </div>
+            <div className='formField'>
+                <label>誕生日</label>
+                <div className='birthDay'>
+                    <select />年
+                    <select />月
+                    <select />日
+                </div>
+            </div>
+            <div className='formField'>
+                <label>年齢</label>
+                <div>※自動計算※</div>
+            </div>
+        </div>
+    )
 }
 
 export default CustomerDetail
